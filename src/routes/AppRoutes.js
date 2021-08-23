e264f3160e3e662eea16b9e61ed1a67328308861
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-23 11:30:25 -0500

    style: estilos del AppRoutes

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -7,12 +7,14 @@ import { AuthRoutes } from './AuthRoutes';
 export const AppRoutes = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Switch>
-          <Route exact path='/' component={JournalScreen} />
-          <Route path='/auth' component={AuthRoutes} />
-          <Redirect to='/auth/login' />
-        </Switch>
+      <div className='auth__main'>
+        <div className='auth__box-container'>
+          <Switch>
+            <Route exact path='/' component={JournalScreen} />
+            <Route path='/auth' component={AuthRoutes} />
+            <Redirect to='/auth/login' />
+          </Switch>
+        </div>
       </div>
     </BrowserRouter>
   );
