6d3b9609111949b2b97c042010217644ab688522
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-25 22:59:58 -0500

    feat: estructura y diseño de NotesScreen

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,28 @@ export const NoteScreen = () => {
   return (
     <div className='notes__main-content'>
       <NotesAppBar />
+
+      <div className='notes__content'>
+        <input
+          type='text'
+          placeholder='Some awesome title'
+          name='title'
+          className='notes__title-input'
+          autocomplete='off'
+        />
+        <textarea
+          name='description'
+          className='notes__textarea'
+          placeholder='what happened today'
+        ></textarea>
+
+        <div className='notes__image'>
+          <img
+            src='https://enfocando.es/wp-content/2016/08/Panorama_7490.jpg'
+            alt='title-note'
+          />
+        </div>
+      </div>
     </div>
   );
 };
