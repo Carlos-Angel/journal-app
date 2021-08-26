0d77f6e4641fb68e6668dfc715fa2c37a4ca3c2b
Charly_Angel <c.a.a.a.503@gmail.com>
2021-08-25 22:46:15 -0500

    style: estilos de NoteAppBar

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export const NotesAppBar = () => {
+  return (
+    <div className='notes__appbar'>
+      <span>25 de agosto del 2021</span>
+      <div>
+        <button className='btn'>picture</button>
+        <button className='btn'>save</button>
+      </div>
+    </div>
+  );
+};
