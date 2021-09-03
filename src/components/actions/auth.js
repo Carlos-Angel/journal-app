b1716713aaee7363247aa431e7fa74ea512e076a
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 10:48:33 -0500

    feat: action asíncrono

--- src/components/actions/auth.js
+++ src/components/actions/auth.js
@@ -1,5 +1,13 @@
 import { authTypes } from '../types';
 
+export const startLoginWithEmailAndPassword = (email, password) => {
+  return (dispatch) => {
+    setTimeout(() => {
+      dispatch(login(123, 'pedro'));
+    }, 3500);
+  };
+};
+
 export const login = (uuid, displayName) => {
   return {
     type: authTypes.login,
