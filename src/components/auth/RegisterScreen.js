cd504596393df3b38ec97768c56ab100e29ba228
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 17:00:28 -0500

    feat: majeno del formulario con useForm

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,36 +1,58 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from 'hooks/useForm';
 
 export const RegisterScreen = () => {
+  const initialValuesForm = {
+    email: '',
+    name: '',
+    password: '',
+    confirmPassword: '',
+  };
+  const [valuesForm, handleInputChange] = useForm(initialValuesForm);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('values', valuesForm);
+  };
+
   return (
     <>
       <h3 className='auth__title mb-5'>Register</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type='text'
           name='name'
           placeholder='Name'
           className='auth__input'
-          autocomplete='off'
+          autoComplete='off'
+          value={valuesForm.name}
+          onChange={handleInputChange}
         />
         <input
           type='email'
           name='email'
           placeholder='email'
           className='auth__input'
-          autocomplete='off'
+          autoComplete='off'
+          value={valuesForm.email}
+          onChange={handleInputChange}
         />
         <input
           type='password'
           name='password'
           placeholder='password'
           className='auth__input'
+          value={valuesForm.password}
+          onChange={handleInputChange}
         />
         <input
           type='password'
-          name='confirm-password'
+          name='confirmPassword'
           placeholder='Confirm password'
           className='auth__input'
+          value={valuesForm.confirmPassword}
+          onChange={handleInputChange}
         />
         <button className='mb-5 btn btn-primary btn-block' type='submit'>
           Register
