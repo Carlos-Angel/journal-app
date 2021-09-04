46b9de52cce3dc2c195a4d6a1495a0dcaa898b8a
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 23:34:55 -0500

    feat: protección de rutas

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,28 +1,51 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { JournalScreen } from 'components/journal/JournalScreen';
+import { PrivateRoute } from './PrivateRoutes';
+import { PublicRoute } from './PublicRoutes';
+
 import { AuthRoutes } from './AuthRoutes';
 import { login } from 'actions/auth';
 
 export const AppRoutes = () => {
+  const [checking, setChecking] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const dispatch = useDispatch();
+
   useEffect(() => {
     const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
       if (user?.uid) {
         dispatch(login(user.uid, user.displayName));
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
       }
+      setChecking(false);
     });
-  }, [dispatch]);
+  }, [dispatch, setChecking, setIsLoggedIn]);
+
+  if (checking) {
+    return null;
+  }
 
   return (
     <BrowserRouter>
       <div>
         <Switch>
-          <Route exact path='/' component={JournalScreen} />
-          <Route path='/auth' component={AuthRoutes} />
+          <PublicRoute
+            isLogged={isLoggedIn}
+            path='/auth'
+            component={AuthRoutes}
+          />
+          <PrivateRoute
+            isLogged={isLoggedIn}
+            exact
+            path='/'
+            component={JournalScreen}
+          />
           <Redirect to='/auth/login' />
         </Switch>
       </div>
---
+++ src/routes/PrivateRoutes.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export const PrivateRoute = ({ isLogged, component: Component, ...rest }) => {
+  return (
+    <Route
+      {...rest}
+      component={(props) =>
+        isLogged ? <Component {...props} /> : <Redirect to='/auth/login' />
+      }
+    ></Route>
+  );
+};
+
+PrivateRoute.propTypes = {
+  isLogged: PropTypes.bool.isRequired,
+  component: PropTypes.func.isRequired,
+};
