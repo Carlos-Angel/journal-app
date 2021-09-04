2c4ccfc1bb39499b90af93b69eaaf18706b9aee5
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 20:34:36 -0500

    feat: mostrar mensaje de error en el formulario register

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import validator from 'validator';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { removeError, setError } from 'actions/ui';
 import { useForm } from 'hooks/useForm';
 
 export const RegisterScreen = () => {
   const dispatch = useDispatch();
+  const { msgError } = useSelector((state) => state.ui);
+
   const initialValuesForm = {
     email: '',
     name: '',
@@ -44,6 +46,7 @@ export const RegisterScreen = () => {
     <>
       <h3 className='auth__title mb-5'>Register</h3>
       <form onSubmit={handleSubmit}>
+        {msgError && <div className='auth__alert-error'>{msgError}</div>}
         <input
           type='text'
           name='name'
