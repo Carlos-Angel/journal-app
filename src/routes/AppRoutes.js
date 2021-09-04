dc87e1239248776effe4437fa6704e49a58bcdb0
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-03 21:53:24 -0500

    feat: persistir la autenticación del usuario

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-
+import { useDispatch } from 'react-redux';
+import { onAuthStateChanged, getAuth } from 'firebase/auth';
 import { JournalScreen } from 'components/journal/JournalScreen';
 import { AuthRoutes } from './AuthRoutes';
+import { login } from 'actions/auth';
 
 export const AppRoutes = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+      }
+    });
+  }, [dispatch]);
+
   return (
     <BrowserRouter>
       <div>
