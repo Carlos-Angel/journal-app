f13f12b54bcecc96497d6d364ad97eb1bfb3435e
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-08 14:39:18 -0500

    style: agregar animaciones

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -47,7 +47,10 @@ export const RegisterScreen = () => {
   return (
     <>
       <h3 className='auth__title mb-5'>Register</h3>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={handleSubmit}
+        className='animate__animated animate__fadeIn animate__faster'
+      >
         {msgError && <div className='auth__alert-error'>{msgError}</div>}
         <input
           type='text'
