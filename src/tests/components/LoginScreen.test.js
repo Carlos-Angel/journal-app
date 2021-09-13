a6a17c500089049ffb1238d8e528120e9d07eed5
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-12 21:48:19 -0500

    test: pruebas de las acciones startGoogleLogin y startLoginWithEmailAndPassword en LoginScreen

--- src/tests/components/LoginScreen.test.js
+++ src/tests/components/LoginScreen.test.js
@@ -6,6 +6,12 @@ import thunk from 'redux-thunk';
 import '@testing-library/jest-dom';
 import { LoginScreen } from 'components/auth/LoginScreen';
 import { Provider } from 'react-redux';
+import { startGoogleLogin, startLoginWithEmailAndPassword } from 'actions/auth';
+
+jest.mock('actions/auth', () => ({
+  startGoogleLogin: jest.fn(),
+  startLoginWithEmailAndPassword: jest.fn(),
+}));
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
@@ -19,20 +25,37 @@ const initState = {
 };
 
 let store = mockStore(initState);
+store.dispatch = jest.fn();
 
 describe('Pruebas del componente <LoginScreen />', () => {
   beforeEach(() => {
     store.clearActions();
+    jest.clearAllMocks();
   });
 
+  const wrapper = mount(
+    <Provider store={store}>
+      <MemoryRouter>
+        <LoginScreen />
+      </MemoryRouter>
+    </Provider>,
+  );
+
   test('debe de mostrarse correctamente', () => {
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter>
-          <LoginScreen />
-        </MemoryRouter>
-      </Provider>,
-    );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  test('debe de disparar la acción startGoogleLogin', () => {
+    const buttonGoogle = wrapper.find('.google-btn').prop('onClick');
+    buttonGoogle();
+
+    expect(startGoogleLogin).toHaveBeenCalled();
+  });
+
+  test('debe de disparar el startLoginWithEmailAndPassword', () => {
+    const buttonForm = wrapper.find('form').prop('onSubmit');
+    buttonForm({ preventDefault() {} });
+
+    expect(startLoginWithEmailAndPassword).toBeCalledTimes(0);
   });
 });
