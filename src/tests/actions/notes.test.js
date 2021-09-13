5eee55ca4c84c2f5370ef9d9553b96fd82979e01
Charly_Angel <c.a.a.a.503@gmail.com>
2021-09-12 19:21:07 -0500

    test: pruebas asincrionas - startLoadingNotes y startSaveNote

--- src/tests/actions/notes.test.js
+++ src/tests/actions/notes.test.js
@@ -1,28 +1,36 @@
-import { deleteDoc, disableNetwork, doc } from '@firebase/firestore';
-import { startNewNote } from 'actions/notes';
+/** * @jest-environment node */
+import { deleteDoc, disableNetwork, doc, getDoc } from '@firebase/firestore';
 import configureStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
 import { db } from 'app';
+import { startNewNote, startLoadingNotes, startSaveNote } from 'actions/notes';
 import { notesTypes } from 'types';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-const store = mockStore({
+const initState = {
   auth: {
     uid: 'TESTING-UID',
   },
-});
+};
+
+let store = mockStore(initState);
 
 describe('Pruebas con las acciones notes', () => {
-  afterAll(async () => {
-    await disableNetwork(db);
+  afterAll(() => {
+    disableNetwork(db);
   });
 
-  test('debe de crear una nueva nota - startNewNote', async () => {
+  beforeEach(() => {
+    store.clearActions();
+  });
+
+  test('debe de crear una nueva nota startNewNote', async () => {
     await store.dispatch(startNewNote());
 
     const actions = store.getActions();
+    // console.log(actions);
 
     expect(actions[0]).toEqual({
       type: notesTypes.active,
@@ -48,5 +56,42 @@ describe('Pruebas con las acciones notes', () => {
 
     const referenceDocument = doc(db, `TESTING-UID/journal/notes/${docId}`);
     await deleteDoc(referenceDocument);
+  });
+
+  test('startLoadingNotes debe cargar las notas', async () => {
+    await store.dispatch(startLoadingNotes('TESTING-UID'));
+    const actions = store.getActions();
+
+    expect(actions[0]).toEqual({
+      type: notesTypes.load,
+      payload: expect.any(Array),
+    });
+
+    const expected = {
+      id: expect.any(String),
+      title: expect.any(String),
+      body: expect.any(String),
+      date: expect.any(Number),
+    };
+
+    expect(actions[0].payload[0]).toMatchObject(expected);
+  });
+
+  test('startSaveNote debe de actualizarla nota', async () => {
+    const note = {
+      id: '0Yv4JfPrfwzHGa9qUlyB',
+      title: 'titulo editado',
+      body: 'body editado',
+    };
+
+    await store.dispatch(startSaveNote(note));
+    const actions = store.getActions();
+
+    expect(actions[0].type).toBe(notesTypes.update);
+    const docRef = await getDoc(
+      doc(db, `/TESTING-UID/journal/notes/${note.id}`),
+    );
+
+    expect(note.title).toBe(docRef.data().title);
   });
 });
