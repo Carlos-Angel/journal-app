2504c0f3d2c2397246e60496d2df8e4aa67ccc1f
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-09-08 20:56:00 -0500

    perf: remplazar create react app por vite
    - remplazar create react app por vite
    - utilizar la biblioteca materialUI por sass
    - agregar el tema purple
    - agregar estructura por modulos
    - actulización de las dependencias

--- src/tests/types/types.test.js
+++
@@ -1,9 +0,0 @@
-import { authTypesMock, notesTypesMock } from 'tests/mocks/typesMock';
-import { authTypes, notesTypes } from 'types';
-
-describe('Pruebas de nuestros tipos', () => {
-  test('deben de contener estos tipos', () => {
-    expect(authTypes).toEqual(authTypesMock);
-    expect(notesTypes).toEqual(notesTypesMock);
-  });
-});
