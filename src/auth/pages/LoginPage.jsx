1127b13fb25af3ed371d5391b426bb9cffcf6cc1
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-09-11 20:28:31 -0500

    feat: custom hook useForm

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -2,57 +2,71 @@ import { Link as RouterLink } from 'react-router-dom';
 import { Button, Grid, Link, TextField, Typography } from '@mui/material';
 import { Google } from '@mui/icons-material';
 import { AuthLayout } from '../layout/AuthLayout';
-
+import { useForm } from '../../hooks';
 
 export const LoginPage = () => {
+  const { formState, onInputChange } = useForm({ email: 'test@test.com', password: 'password' });
+  const { email, password } = formState;
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
+  const onGoogleSignIn = () => {
+    console.log('onGoogleSignIn');
+  };
+
   return (
-    <AuthLayout title="Login">
-      <form>
-          <Grid container>
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Correo" 
-                type="email" 
-                placeholder='correo@google.com' 
-                fullWidth
-              />
-            </Grid>
-
-            <Grid item xs={ 12 } sx={{ mt: 2 }}>
-              <TextField 
-                label="Contraseña" 
-                type="password" 
-                placeholder='Contraseña' 
-                fullWidth
-              />
-            </Grid>
-            
-            <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
-                  Login
-                </Button>
-              </Grid>
-              <Grid item xs={ 12 } sm={ 6 }>
-                <Button variant='contained' fullWidth>
-                  <Google />
-                  <Typography sx={{ ml: 1 }}>Google</Typography>
-                </Button>
-              </Grid>
-            </Grid>
-
-
-            <Grid container direction='row' justifyContent='end'>
-              <Link component={ RouterLink } color='inherit' to="/auth/register">
-                Crear una cuenta
-              </Link>
-            </Grid>
-
+    <AuthLayout title='Login'>
+      <form onSubmit={onSubmit}>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Correo'
+              type='email'
+              placeholder='correo@google.com'
+              name='email'
+              value={email}
+              onChange={onInputChange}
+              fullWidth
+            />
           </Grid>
 
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label='Contraseña'
+              type='password'
+              placeholder='Contraseña'
+              name='password'
+              value={password}
+              onChange={onInputChange}
+              fullWidth
+            />
+          </Grid>
 
-        </form>
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} sm={6}>
+              <Button type='submit' variant='contained' fullWidth>
+                Login
+              </Button>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Button variant='contained' fullWidth onClick={onGoogleSignIn}>
+                <Google />
+                <Typography sx={{ ml: 1 }}>Google</Typography>
+              </Button>
+            </Grid>
+          </Grid>
 
+          <Grid container direction='row' justifyContent='end'>
+            <Link component={RouterLink} color='inherit' to='/auth/register'>
+              Crear una cuenta
+            </Link>
+          </Grid>
+        </Grid>
+      </form>
     </AuthLayout>
-  )
-}
+  );
+};
