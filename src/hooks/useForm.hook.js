f30acaca23f4190475464f17959e4c64e60fcfda
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-10-16 17:19:44 -0500

    feat: actualizar el estado inicial del useForm cuando cambie

--- src/hooks/useForm.hook.js
+++ src/hooks/useForm.hook.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export default function useForm(initialForm = {}) {
   const [formState, setFormState] = useState(initialForm);
@@ -10,6 +10,10 @@ export default function useForm(initialForm = {}) {
       [name]: value,
     });
   };
+
+  useEffect(() => {
+    setFormState(initialForm);
+  }, [initialForm]);
 
   const onResetForm = () => {
     setFormState(initialForm);
